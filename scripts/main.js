ad5c9110b7800fbad57caca19bea0c2e65bf62e3
Cody Coleman <colemanc@mit.edu>
2013-03-17 12:06:31 -0400

    Finished CSS changes adding tag ffunctionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,99 @@
 
 $(document).ready(function(){
-
 	$("a[rel*=leanModal]").leanModal();
+	
+	addAction();
 
-	$("a[rel*=leanModal]").click(function(e){
+
+
+	// Put the current time from the video into start input box	
+	$(".bl-start").click(function(e){
+		var curr_time = player.getCurrentTime();
+		$("input[name='bl-start']").val(curr_time);
+
+		// Generate new "a" Tag with snippet in bl-URL
+		generateTag();
+	}); 
+
+	// Put the current time from the video into end input box
+	$(".bl-end").click(function(e){
+		var curr_time = player.getCurrentTime();
+		$("input[name='bl-end']").val(curr_time);
+
+		// Generate new "a" Tag with snippet in bl-URL
+		generateTag();
+	}); 
+
+	// Play the snippet from the start and end input boxes
+	$(".bl-play").click(function(e){
+		var start = $("input[name='bl-start']").val();
+		var end = $("input[name='bl-end']").val();
+		var url = player.getVideoUrl();
+		var vid = getVideoIdFromURL(url);
+
+		player.loadVideoById({'videoId': vid, 'startSeconds': start, 'endSeconds': end, 'suggestedQuality': 'large'});
+	});
+
+	// Reset the video player
+	$(".bl-reset").click(function(e){
+		var url = player.getVideoUrl();
+		var vid = getVideoIdFromURL(url);
+		clearInputs();
+
+		player.loadVideoById(vid, 0, "large")
+	});
+
+});
+
+function addAction(){
+		$("a[rel*=leanModal]").click(function(e){
 		
-		// Get source URL for video
-		var url = $(this).attr('data-u');
 
-		// Update source URL in the modal dialog box
-		$("#bl iframe").attr('src', url);
-		$(".bl-srcURL").attr('href', url);
-		$(".bl-srcURL").text(url);
+		if($(this).attr('data-u') !== undefined){
+			// Get source URL for video
+			var url = $(this).attr('data-u');
 
-		// clear input fields
-		$("input[name='bl-start']").val("");
-		$("input[name='bl-end']").val("");
+			// Update source URL in the modal dialog box
+			$("#bl iframe").attr('src', url);
+			$(".bl-srcURL").attr('href', url);
+			$(".bl-srcURL").text(url);
+
+			clearInputs();
+		}else if($(this).attr('data-vid') !== undefined){
+			var vid = $(this).attr('data-vid');
+			var start = $(this).attr('data-start');
+			var end = $(this).attr('data-end');
+			var type = $(this).attr('data-type');
+			var url = "http://www.youtube.com/embed/"+vid;
+
+			$("#bl-vid iframe").attr('src', url);
+			playerV.loadVideoById({'videoId': vid, 'startSeconds': start, 'endSeconds': end, 'suggestedQuality': 'large'});
+
+
+		}
 
 	});
 
-	// Generate a URL for the snippet from the start and end input boxes
-	$(".bl-create").click(function(e){
-		var start_time = $("input[name='bl-start']").val();
-		var end_time = $("input[name='bl-end']").val();
-		// var url = $("#bl iframe").attr('src');
+}
+
+
+
+// clear input fields
+function clearInputs(){
+	$("input[name='bl-end']").val('');
+	$("input[name='bl-start']").val('');
+	$("input[name='bl-start']").removeClass("bl-incorrect");
+	$("input[name='bl-end']").removeClass("bl-incorrect");
+	$(".bl-URL").text("Generate URL goes here")
+}
+
+// Generate a URL for the snippet from the start and end input boxes
+function generateTag() {
+	var start_time = $("input[name='bl-start']").val();
+	var end_time = $("input[name='bl-end']").val();
+	if ((start_time < end_time || end_time === '') && (start_time !== '')) {
+		$("input[name='bl-start']").removeClass("bl-incorrect");
+		$("input[name='bl-end']").removeClass("bl-incorrect");
 		var url = player.getVideoUrl();
 		var vid = getVideoIdFromURL(url);
 		var type = 'yt'
@@ -35,43 +105,21 @@ $(document).ready(function(){
 					  "' href='#bl-vid'>Click Here</a>";
 
 		$(".bl-URL").text(newLink);
-	});
 
 
-	// Put the current time from the video into start input box	
-	$(".bl-start").click(function(e){
-		var curr_time = player.getCurrentTime();
-		$("input[name='bl-start']").val(curr_time);
-	}); 
+		// For testing only
+		$(".bl-test a").remove();
+		$(".bl-test").append(newLink);
 
-	// Put the current time from the video into end input box
-	$(".bl-end").click(function(e){
-		var curr_time = player.getCurrentTime();
-		$("input[name='bl-end']").val(curr_time);
-	}); 
+		$("a[rel*=leanModal]").leanModal();
+		addAction();
 
-	// Play the snippet from the start and end input boxes
-	$(".bl-play").click(function(e){
-		var start = $("input[name='bl-start']").val();
-		var end = $("input[name='bl-end']").val();
-		var url = player.getVideoUrl();
-		var vid = getVideoIdFromURL(url);
+	}else{
+		$("input[name='bl-start']").addClass("bl-incorrect");
+		$("input[name='bl-end']").addClass("bl-incorrect");
+	}
+}
 
-		$(".bl-URL").text(vid);
-
-		player.loadVideoById({'videoId': vid, 'startSeconds': start, 'endSeconds': end, 'suggestedQuality': 'large'})
-		player.seekTo(start, true);
-	});
-
-	// Reset the video player
-	$(".bl-reset").click(function(e){
-		var url = player.getVideoUrl();
-		var vid = getVideoIdFromURL(url);
-
-		player.loadVideoById(vid, 0, "large")
-	});
-
-});
 
 // This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
@@ -91,6 +139,12 @@ function onYouTubeIframeAPIReady() {
 	  	// set up event listeners
 	  }
 	});
+
+	playerV = new YT.Player('playerV', {
+          events: {
+
+          }
+    });
 };
 
 function getVideoIdFromURL(url){
