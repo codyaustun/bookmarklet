838ef43314d3092be287c1158786c0c4d26750eb
codyaustun <colemanc@mit.edu>
2013-03-22 01:02:33 -0400

    added comments and specifications

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,63 @@
 var bookMarklet = 
 {	
+	// bookMarklet namespace
+	// Variables:
+	//	 player
+	//	 playerV
+
+	// Functions:
+	//	 start()
+	//	 addAction()
+	//	 clearInputs()
+	//	 getVideoIdFromURL(url)
+	// 	 generateTag()
+	//	 onPlayerReady()
+	// 	 setup()
+
+	// Sets up event listeners for ".bl-start", ".bl-end", ".bl-reset", and
+	// 	".bl-done"
+	// Creates bookMarklet.player
+	// Through addAction(), Adds click listeners to all #bl and 
+	// 	#bl-vid elements
+	// @parameters: none
+	// @returns: nil
+	// @modifies: ".bl-start", ".bl-end", ".bl-reset", ".bl-done", "#bl", and
+	// 	"#bl-elements"
 	start: function (){
+
+		// Adds click listeners to all #bl and #bl-vid elements
 		bookMarklet.addAction();
 
 		// Create a YouTube player object for the modal dialog window
-		// alert(bookMarklet.player);
 		bookMarklet.player = new window.YT.Player('player', {
 		  events: {
 		  	// set up event listeners
+		  	// Note: add event listeners when the was implmented
 		  }
 		});
 
-		// Put the current time from the video into start input box	
+
+		// "Start Time" button in #bl box
 		$(".bl-start").click(function(e){
+
+			// Put the current time from the video into start input box	
 			var curr_time = bookMarklet.player.getCurrentTime();
 			$("input[name='bl-start']").val(curr_time);
 
 			// Generate new "a" Tag with snippet in bl-URL
+			// Add tag to bl-answer for current question
 			bookMarklet.generateTag();
 		}); 
 
-		// Put the current time from the video into end input box
+		// "End Time" button in #bl box
 		$(".bl-end").click(function(e){
+
+			// Put the current time from the video into end input box
 			var curr_time = bookMarklet.player.getCurrentTime();
 			$("input[name='bl-end']").val(curr_time);
 
 			// Generate new "a" Tag with snippet in bl-URL
+			// Add tag to bl-answer for current question
 			bookMarklet.generateTag();
 		}); 
 
@@ -40,53 +72,78 @@ var bookMarklet =
 		// 	player.loadVideoById({'videoId': vid, 'startSeconds': start, 'endSeconds': end, 'suggestedQuality': 'large'});
 		// });
 
-		// close modal window and stop video
+		// "Done" button in #bl box
+		// Also, closes #bl
 		$(".bl-done").click(function(e){
+
+			// Stop "player" playback
 			bookMarklet.player.stopVideo();
 		});
 
-		// Reset the video player
+		// "Reset" button in #bl box
 		$(".bl-reset").click(function(e){
+
+			// Reset the video player to initial state
 			var url = bookMarklet.player.getVideoUrl();
 			var vid = bookMarklet.getVideoIdFromURL(url);
 			bookMarklet.clearInputs();
-
 			bookMarklet.player.loadVideoById(vid, 0, "large")
 		});
 	},
 
 
+	// Adds click listeners to all #bl and #bl-vid leanModal links
+	// "a" tag with "data-u" attribute correspond to snippet generate links
+	// "a" tag with "data-vid" attribute correspond to snippet playback links
+	// @parameters: none
+	// @returns: nil
+	// @modifies: all "a[rel*=leanModal]" with attr 'data-u' or 'data-vid'
 	addAction: function(){
-			$("a[rel*=leanModal]").click(function(e){
+		$("a[rel*=leanModal]").click(function(e){
 			
 
 			if($(this).attr('data-u') !== undefined){
-				// Get source URL for video
+				// Get source URL for video from data-u
 				var url = $(this).attr('data-u');
 
-				// Update source URL in the modal dialog box
+				
+				// Important: loads video into iframe and starts "player" controls
 				$("#bl iframe").attr('src', url);
+
+				// Update source URL in the modal dialog box
 				$(".bl-srcURL").attr('href', url);
 				$(".bl-srcURL").text(url);
 
+				// Reset the video player to initial state
 				bookMarklet.clearInputs();
+
 			}else if($(this).attr('data-vid') !== undefined){
+
+				// Get video information from snippet playback link
 				var vid = $(this).attr('data-vid');
 				var start_time = $(this).attr('data-start');
 				var end_time = $(this).attr('data-end');
 				var type = $(this).attr('data-type');
+
+				// Generate URL needed by iframes
 				var url = "http://www.youtube.com/embed/"+vid;
 
+				// Important: loads video into iframe and starts "playerV" controls
 				$("#bl-vid iframe").attr('src', url);
+
+				// Stores the start and end times in the iframe for playback
+			    $("#bl-vid iframe").attr('data-start', start_time);
+			    $("#bl-vid iframe").attr('data-end', end_time);
+
+				// Create a YouTube player object for the modal dialog window
 				playerV = new YT.Player('playerV', {
 			          events: {
+			          	// Once "playerV" is ready this cues the snippet to play
 			          	'onReady': bookMarklet.onPlayerReady,
 			          }
 			    });
 			
 
-			    $("#bl-vid iframe").attr('data-start', start_time);
-			    $("#bl-vid iframe").attr('data-end', end_time);
 
 
 			}
@@ -95,7 +152,10 @@ var bookMarklet =
 
 	},
 
-	// clear input fields
+	// Clear input fields in #bl box
+	// @parameters: none
+	// @returns: nil
+	// @modifies: all "input[name='bl-end']", "input[name='bl-start']", and ".bl-URL"
 	clearInputs: function(){
 		$("input[name='bl-end']").val('');
 		$("input[name='bl-start']").val('');
@@ -105,6 +165,11 @@ var bookMarklet =
 	},
 
 	// Generate a URL for the snippet from the start and end input boxes
+	// @parameters: none
+	// @returns: nil
+	// @modifies: all "input[name='bl-start']", "input[name='bl-end']", ".bl-URL"
+	// 			  one ".bl-answer"
+	// @creates: one "a[rel*=leanModal]" with attr "data-vid"
 	generateTag: function() {
 		var start_time = $("input[name='bl-start']").val();
 		var end_time = $("input[name='bl-end']").val();
@@ -128,10 +193,6 @@ var bookMarklet =
 			$(srcQues).prev(".bl-answer").children().remove();
 			$(srcQues).prev(".bl-answer").append(newLink);
 
-			// For testing only
-			//$(".bl-answer a").remove();
-			// $(".bl-answer").append(newLink);
-
 			// adds overlay
 			$("a[rel*=leanModal]").leanModal();
 			bookMarklet.addAction();
@@ -142,7 +203,15 @@ var bookMarklet =
 		}
 	},
 
+	// Gets video id from v=VIDEO_ID and embed/VIDEO_ID links
+	// Example: "http://www.youtube.com/embed/8f7wj_RcqYk" -> "8f7wj_RcqYk"
+	// Example: "http://www.youtube.com/watch?v=3YIx3vARyEs" -> "3YIx3vARyEs"
+	// @parameters: url - String for the url to a video
+	// @returns: vid - String for the videos id
+	// @modifies: None
 	getVideoIdFromURL: function(url){
+
+		// handles v=VIDEO_ID and embed/VIDEO_ID links
 		if(url.indexOf("v=") !== -1){
 			var vid = url.split('v=')[1];
 			var ampersandPosition = vid.indexOf('&');
@@ -162,6 +231,12 @@ var bookMarklet =
 		}
 	},
 
+	// Cues Video in "playerV" to data-start and data-end from #bl-vi iframe
+	// data-start and data-en are generated from an "a" tag with "data-vid" attribute.
+	// See bookMarklet.addAction() 
+	// @parameters: event - onReady event from playerV
+	// @returns: nil
+	// @modifies: all "#bl-vid iframe"
 	onPlayerReady: function(event) {
 			var start_time = $("#bl-vid iframe").attr('data-start');
 			var end_time = $("#bl-vid iframe").attr('data-end');
@@ -170,9 +245,10 @@ var bookMarklet =
 	        event.target.cueVideoById({'videoId': vid, 'startSeconds': start_time, 'endSeconds': end_time, 'suggestedQuality': 'large'});
 	},
 
-	player: 'foo',
-	playerV: 'bar',
-
+	// Add scripts for YouTube iFrame API
+	// @parameters: none
+	// @returns: nil
+	// @modifies: "document"
 	setup: function(){
 		// This code loads the IFrame Player API code asynchronously.
 		var tag = document.createElement('script');
